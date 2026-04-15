52ec8dfd46813de0473a3731978c6fdf6f5414b3
Rizquan Mohamed <mohammedrizquan10@gmail.com>
2026-04-15 10:19:08 +0530

    Update on Event Class

--- src/main/java/com/logistic/common/entity/Consignment.java
+++ src/main/java/com/logistic/common/entity/Consignment.java
@@ -21,14 +21,6 @@ public class Consignment {
 
     private String status;
 
-    private Float weight;
-
-    private Float height;
-
-    private Float length;
-
-    private Float width;
-
     private LocalDateTime createdDate;
 
     private String createdBy;
--- src/main/java/com/logistic/common/entity/Event.java
+++ src/main/java/com/logistic/common/entity/Event.java
@@ -21,6 +21,14 @@ public class Event {
 
     private String description;
 
+    private Float weight;
+
+    private Float height;
+
+    private Float length;
+
+    private Float width;
+
     private LocalDateTime createdDate;
 
     private String createdBy;
