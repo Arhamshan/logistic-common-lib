9de09ca9779e059e82d6c48183e1aa3e39434745
Rizquan Mohamed <mohammedrizquan10@gmail.com>
2026-07-06 15:37:30 +0530

    AssignDriver table and entity

---
+++ src/main/java/com/logistic/common/entity/DeliveryAssignment.java
@@ -0,0 +1,32 @@
+package com.logistic.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryAssignment {
+
+    private Long id;
+
+    private Long itemId;
+
+    private Long driverUserId;
+
+    private String assignedBy;
+
+    private LocalDateTime assignedDatetime;
+
+    private String status;
+
+    private String remarks;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+}
